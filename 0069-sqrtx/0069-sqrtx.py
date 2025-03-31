da402606539ab19c8c356032237dd2653f597826
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-03-31 19:09:44 +0530

    Time: 0 ms (100%), Space: 12.5 MB (52.33%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,7 +1,5 @@
 class Solution(object):
     def mySqrt(self, x):
-        """
-        :type x: int
-        :rtype: int
-        """
-        
+        ans = x**0.5
+        ans = int(ans)
+        return ans

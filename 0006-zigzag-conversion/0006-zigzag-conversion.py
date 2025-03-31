11de92bb56267517cc0467ddd605d5433c8bed46
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-03-31 19:04:11 +0530

    Time: 19 ms (51.04%), Space: 12.5 MB (83.37%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -1,14 +1,16 @@
 class Solution(object):
     def convert(self, s, numRows):
-        if numRows == 1: return s
-        res = ""
-        for r in range(numRows):
-            increment = 2*(numRows-1)
-            for i in range(r, len(s), increment):
-                res += s[i]
-                if r > 0 and r < numRows - 1 and i + increment - 2 * r < len(s):
-                    res += s[i+increment - 2 * r]
-        return res
-            
+        if numRows == 1:
+            return s
 
-        
+        result = []
+        increment = 2 * (numRows - 1)
+
+        for r in range(numRows):
+            for i in range(r, len(s), increment):
+                result.append(s[i])
+                # Add the diagonal elements for middle rows
+                if r > 0 and r < numRows - 1 and i + increment - 2 * r < len(s):
+                    result.append(s[i + increment - 2 * r])
+
+        return ''.join(result)

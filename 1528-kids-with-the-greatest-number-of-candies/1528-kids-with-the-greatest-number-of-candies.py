014f81936cc40691e0092e992796800aa7dce20c
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-04-04 19:24:33 +0530

    Time: 0 ms (100%), Space: 12.5 MB (12.08%) - LeetHub

--- 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
+++ 1528-kids-with-the-greatest-number-of-candies/1528-kids-with-the-greatest-number-of-candies.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
         maxi = max(candies)
         for i in range(len(candies)):
-            candies[i] = candies[i] + extraCandies>=maxi
-                
-        # ans = [True if candy >= maxi else False for candy in candies]
-        
+            candies[i] = candies[i] + extraCandies >= maxi
         return candies
         

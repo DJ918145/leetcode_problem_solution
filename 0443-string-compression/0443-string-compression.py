05143290e99c43899eb68a73e3ae13b93492b573
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-04-15 17:48:34 +0530

    Time: 3 ms (61.85%), Space: 12.5 MB (79.69%) - LeetHub

--- 0443-string-compression/0443-string-compression.py
+++ 0443-string-compression/0443-string-compression.py
@@ -4,26 +4,23 @@ class Solution(object):
         :type chars: List[str]
         :rtype: int
         """
-        write = 0  # Position to write the compressed character
-        read = 0   # Position to read the character
-        
-        while read < len(chars):
-            char = chars[read]
+        n = len(chars)
+        idx = 0
+        i = 0
+        while i < n:
+            ch = chars[i]
             count = 0
-            
-            # Count the number of occurrences of the same character
-            while read < len(chars) and chars[read] == char:
-                read += 1
+            while i < n and chars[i] == ch:
                 count += 1
-            
-            # Write the character
-            chars[write] = char
-            write += 1
-            
-            # Write the count (if more than 1)
-            if count > 1:
+                i += 1
+            if count == 1:
+                chars[idx] = ch
+                idx += 1
+            else:
+                chars[idx] = ch
+                idx += 1
                 for digit in str(count):
-                    chars[write] = digit
-                    write += 1
-        
-        return write
+                    chars[idx] = digit
+                    idx += 1
+        chars[:] = chars[:idx]
+        return idx

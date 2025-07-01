0bcf938c6ecbdb8180cbadfe344d1759df02b7d6
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-07-01 19:45:29 +0530

    Time: 91 ms (86.34%), Space: 15.1 MB (51.97%) - LeetHub

---
+++ 0918-maximum-sum-circular-subarray/0918-maximum-sum-circular-subarray.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def maxSubarraySumCircular(self, nums):
+        total = sum(nums)
+        max_sum = cur_max = nums[0]
+        min_sum = cur_min = nums[0]
+
+        for n in nums[1:]:
+            cur_max = max(n, cur_max+n)
+            max_sum = max(max_sum, cur_max)
+
+            cur_min = min(n, cur_min+n)
+            min_sum = min(min_sum, cur_min)
+
+        if max_sum<0:
+            return max_sum
+        
+        return max(max_sum, total-min_sum)

8789422a47648ab8debee5da6694d9ee82335c66
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-10-09 21:33:13 +0530

    Time: 3 ms (20.3%), Space: 41.7 MB (61.87%) - LeetHub

--- 2480-find-subarrays-with-equal-sum/2480-find-subarrays-with-equal-sum.java
+++ 2480-find-subarrays-with-equal-sum/2480-find-subarrays-with-equal-sum.java
@@ -1,18 +1,18 @@
 class Solution {
     public boolean findSubarrays(int[] nums) {
         int n = nums.length;
-        boolean answer=false;
+        // boolean answer=false;
         if(n<=2)
-            return answer;
+            return false;
         int sum = 0;
         for(int i=0;i<n-2;i++){
             sum = nums[i]+nums[i+1];
             for(int j=i+1;j<n-1;j++){
                 if(nums[j]+nums[j+1]==sum){
-                    answer = true;
+                    return true;
                 }
             }
         }
-        return answer;
+        return false;
     }
 }

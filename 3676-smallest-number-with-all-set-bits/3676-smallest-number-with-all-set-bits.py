20803dae9bceb4438020dbf143454295949cea79
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-10-29 20:50:46 +0530

    Time: 0 ms (100%), Space: 12.3 MB (98.75%) - LeetHub

---
+++ 3676-smallest-number-with-all-set-bits/3676-smallest-number-with-all-set-bits.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def smallestNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        binary = bin(n)[2:]
+        ans = '1'*len(binary)
+        return int(ans,2)

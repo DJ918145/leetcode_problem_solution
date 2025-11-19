65ca6222cb3989489dc3caa32ee7528b64322f96
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-11-19 20:00:26 +0530

    Time: 4 ms (81.95%), Space: 13.6 MB (90.83%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -4,16 +4,8 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        my_dict = {}
-        for number in nums:
-            if number in my_dict:
-                my_dict[number]+=1
-            else:
-                my_dict[number]=1
-        max_num = 0
-        maxi = 0
-        for key in my_dict:
-            if my_dict[key]>maxi:
-                maxi = my_dict[key]
-                max_num = key
-        return max_num
+        sets = set(nums)
+        for i in sets:
+            if nums.count(i)> len(nums)//2:
+                return i
+        

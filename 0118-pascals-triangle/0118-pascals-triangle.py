f64fa8fd2dbdf1c287fdee7ac2be5d9a77d11503
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2025-12-14 19:05:49 +0530

    Time: 0 ms (100%), Space: 12.4 MB (89.52%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -1,15 +1,18 @@
-class Solution:
-    def generate(self, num):
-        if num == 0: #if inserted number is 0
+class Solution(object):
+    def generate(self, numRows):
+        """
+        :type numRows: int
+        :rtype: List[List[int]]
+        """
+        if numRows == 0: # if the value of numRows is 0 then return []
             return []
-        if num == 1: #if inserted number is 1
+        elif numRows == 1: #if the value of numRows is 1 then return [[1]]
             return [[1]]
+        else:
+            prevRows = self.generate(numRows - 1) # call the recursion process for generating the previous row in the triangle.
+            newRow = [1] * numRows #generate the current row of pascal triangle and initialize with 1.
+            for i in range(1, numRows - 1):
+                newRow[i] = prevRows[-1][i-1] + prevRows[-1][i] #assigning  the value to the row.
+            prevRows.append(newRow) # appending the current row in the previous Rows List.
+            return prevRows #Returning the previous row
         
-        prev = self.generate(num - 1) #recursion in this row
-        newRow = [1] * num #insert 1 in new row
-        
-        for i in range(1, num - 1):
-            newRow[i] = prev[-1][i - 1] + prev[-1][i] #adding elements in row
-        
-        prev.append(newRow)
-        return prev

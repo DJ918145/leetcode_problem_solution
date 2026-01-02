7cfc66d6185d0293f7d8ba0c8a4d13ca13d76431
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-01-02 15:58:37 +0530

    Time: 1 ms (51.65%), Space: 13.4 MB (61.54%) - LeetHub

--- 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.py
@@ -4,10 +4,6 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        repeted = 0
-        answer = 0
-        for i in set(nums):
-            if nums.count(i)>repeted:
-                answer = i
-                repeted = nums.count(i)
-        return answer
+        for i in range(len(nums)/2+1):
+            if nums.count(nums[i])>1:
+                return nums[i]

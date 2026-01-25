db38eb6348f766964b5c162d759f95ceac4d4a72
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-01-25 17:27:25 +0530

    Time: 59 ms (19.58%), Space: 12.8 MB (18.75%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,19 +1,6 @@
 class Solution(object):
-    def canConstruct(self, stri1, stri2):
-        count1 = {}
-        count2 = {}
-        for char in stri1:
-            if char in count1:
-                count1[char] += 1
-            else:
-                count1[char] = 1
-        for char in stri2:
-            if char in count2:
-                count2[char] += 1
-            else:
-                count2[char] = 1
-        for char in count1:
-            if char not in count2 or count1[char] > count2[char]:
-                return False
-        
-        return True
+    def canConstruct(self, ransomNote, magazine):
+        st1, st2 = Counter(ransomNote), Counter(magazine)
+        if st1 & st2 == st1:
+            return True
+        return False

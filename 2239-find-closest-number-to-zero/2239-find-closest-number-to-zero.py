1dc8369247cfd8de613dd8b77e1849afd6bd3fba
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-02-14 18:19:20 +0530

    Time: 11 ms (38.29%), Space: 12.6 MB (77.82%) - LeetHub

--- 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
+++ 2239-find-closest-number-to-zero/2239-find-closest-number-to-zero.py
@@ -4,8 +4,4 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        ans = nums[0]
-        for i in nums:
-            if abs(i) < abs(ans) or (abs(i) == abs(ans) and i > ans):
-                ans = i
-        return ans
+        return min(nums, key=lambda x: (abs(x), -x))

356f484a7eea791f3b035029cc12b8f8ecdec6f7
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-02-14 19:09:45 +0530

    Time: 19 ms (98.91%), Space: 12.5 MB (32.35%) - LeetHub

--- 0018-4sum/0018-4sum.py
+++ 0018-4sum/0018-4sum.py
@@ -1,35 +1,58 @@
 class Solution(object):
     def fourSum(self, nums, target):
+        if not nums or len(nums) < 4:
+            return []
+        
         nums.sort()
-        res = []
+        result = []
         n = len(nums)
-
-        for i in range(n-3):
-            if i > 0 and nums[i] == nums[i-1]:
+        if nums[0] + nums[1] + nums[2] + nums[3] > target:
+            return []
+        
+        if nums[-1] + nums[-2] + nums[-3] + nums[-4] < target:
+            return []
+        
+        for i in range(n - 3):
+            if nums[i] + nums[i+1] + nums[i+2] + nums[i+3] > target:
+                break
+            
+            if nums[i] + nums[-3] + nums[-2] + nums[-1] < target:
                 continue
-
-            for j in range(i+1, n-2):
-                if j > i+1 and nums[j] == nums[j-1]:
+            
+            if i > 0 and nums[i] == nums[i - 1]:
+                continue
+            for j in range(i + 1, n - 2):
+                if nums[i] + nums[j] + nums[j+1] + nums[j+2] > target:
+                    break
+                
+                if nums[i] + nums[j] + nums[-2] + nums[-1] < target:
                     continue
-
-                l, r = j+1, n-1
-
-                while l < r:
-                    s = nums[i] + nums[j] + nums[l] + nums[r]
-
-                    if s == target:
-                        res.append([nums[i], nums[j], nums[l], nums[r]])
-                        l += 1
-                        r -= 1
-
-                        while l < r and nums[l] == nums[l-1]:
-                            l += 1
-                        while l < r and nums[r] == nums[r+1]:
-                            r -= 1
-
-                    elif s < target:
-                        l += 1
+                
+                if j > i + 1 and nums[j] == nums[j - 1]:
+                    continue
+                
+                left = j + 1
+                right = n - 1
+                needed_sum = target - nums[i] - nums[j]
+                
+                while left < right:
+                    current_sum = nums[left] + nums[right]
+                    
+                    if current_sum == needed_sum:
+                        result.append([nums[i], nums[j], nums[left], nums[right]])
+                        
+                        while left < right and nums[left] == nums[left + 1]:
+                            left += 1
+                        
+                        while left < right and nums[right] == nums[right - 1]:
+                            right -= 1
+                        
+                        left += 1
+                        right -= 1
+                        
+                    elif current_sum < needed_sum:
+                        left += 1
                     else:
-                        r -= 1
-
-        return res
+                        right -= 1
+        
+        return result

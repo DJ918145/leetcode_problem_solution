e7f6039ca975641c9d2c233f3e5dc3e7e81f652d
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-02-21 19:36:22 +0530

    Time: 23 ms (29.15%), Space: 8 MB (35.12%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.cpp
@@ -1,21 +1,30 @@
 class Solution {
 public:
-    // Since set bits for an int can't exceed 32, 
-    // we can use a simple lookup or a small helper.
-    bool isSmallPrime(int n) {
-        return (n == 2 || n == 3 || n == 5 || n == 7 || n == 11 || 
-                n == 13 || n == 17 || n == 19 || n == 23 || n == 29 || n == 31);
+    bool isPrime(long long n) {
+        if (n <= 1) return false;
+        if (n <= 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (long long i = 5; i * i <= n; i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+        }
+        return true;
+    }
+
+    bool intToBinary(int n){
+        int count = 0;
+        while(n>0){
+            if(n%2==1) count++;
+            n/=2;
+        }
+        return isPrime(count);
     }
 
     int countPrimeSetBits(int left, int right) {
-        int totalCount = 0;
-        for (int i = left; i <= right; i++) {
-            // __builtin_popcount is a fast, built-in C++ function to count set bits
-            int setBits = __builtin_popcount(i);
-            if (isSmallPrime(setBits)) {
-                totalCount++;
-            }
+        int count = 0;
+        for(int i = left; i<=right; i++){
+            if(intToBinary(i)) count++;
         }
-        return totalCount;
+        return count;
     }
 };

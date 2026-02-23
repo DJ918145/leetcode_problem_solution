9c2903ae2a32b93c9642f5178e24e748fc8a9ee6
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-02-23 22:17:47 +0530

    Time: 0 ms (100%), Space: 12.5 MB (61.16%) - LeetHub

--- 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
+++ 2396-strictly-palindromic-number/2396-strictly-palindromic-number.py
@@ -1,12 +1,7 @@
 class Solution(object):
-    def isPalindrome(self, n, d):
-        st = ''
-        while n>0:
-            st+= str(n%d)
-            n//=d
-        return True if st==st[::-1] else False
     def isStrictlyPalindromic(self, n):
-        for i in range(2,n+1):
-            if(self.isPalindrome(n, i)==False):
-                return False
-        return True
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return False

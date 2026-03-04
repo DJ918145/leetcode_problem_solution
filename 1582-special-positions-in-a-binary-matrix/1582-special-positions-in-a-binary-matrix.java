3cfee541765f679aff51f711629d1060394112cd
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-03-04 13:12:59 +0530

    Time: 2 ms (94.84%), Space: 47.1 MB (83.45%) - LeetHub

--- 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
+++ 1582-special-positions-in-a-binary-matrix/1582-special-positions-in-a-binary-matrix.java
@@ -1,28 +1,22 @@
 class Solution {
-    public boolean isRow(int[][] mat, int i, int j){
-        for(int row = 0; row<mat[0].length; row++){
-            if(row == j) continue;
-            else{
-                if(mat[i][row]==1) return false;
-            }
-        }
-        return true;
-    }
-    public boolean isCol(int[][] mat, int i, int j){
-        for(int col = 0;col<mat.length; col++){
-            if(col==i) continue;
-            else{
-                if(mat[col][j]==1) return false;
-            }
-        }
-        return true;
-    }
     public int numSpecial(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[] rowSum = new int[m];
+        int[] colSum = new int[n];
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 1){
+                    rowSum[i]++;
+                    colSum[j]++;
+                }
+            }
+        }
         int count = 0;
-        for(int row = 0;row<mat.length; row++){
-            for(int col=0;col<mat[0].length;col++){
-                if(mat[row][col]==1){
-                    if(isRow(mat, row, col) && isCol(mat, row, col)) count++;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 1 && rowSum[i] == 1 && colSum[j] == 1){
+                    count++;
                 }
             }
         }

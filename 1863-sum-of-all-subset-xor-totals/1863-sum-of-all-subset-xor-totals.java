3057aa54e3fa06cd661d4397b4bd485b8b51bf23
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-03-07 18:29:09 +0530

    Time: 0 ms (100%), Space: 42.8 MB (39.14%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -1,24 +1,10 @@
 class Solution {
     public int subsetXORSum(int[] nums) {
-        int xor=0;
-        List<List<Integer>>outer=new ArrayList<>();
-        outer.add(new ArrayList<>());
-        for(int num:nums){
-            int n=outer.size();
-            for(int i=0;i<n;i++){
-                List<Integer>internal=new ArrayList<>(outer.get(i));
-                internal.add(num);
-                outer.add(internal);
-            }
+        int orResult = 0;
+        for (int num : nums) {
+            orResult |= num;
         }
-        for(int i=0;i<outer.size();i++){
-            int temp=0;
-            for(int j=0;j<outer.get(i).size();j++){
-                temp=temp^outer.get(i).get(j);
-            }
-            xor+=temp;
-        }
-    return xor;
+        // Result is (OR of all elements) * 2^(n-1)
+        return orResult << (nums.length - 1);
     }
-    
 }

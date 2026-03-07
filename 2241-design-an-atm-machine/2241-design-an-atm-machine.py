9f2288e2b6de2f2b88a245d191394cb40a819c5c
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-03-07 21:06:20 +0530

    Time: 20 ms (73.32%), Space: 22 MB (9.87%) - LeetHub

--- 2241-design-an-atm-machine/2241-design-an-atm-machine.py
+++ 2241-design-an-atm-machine/2241-design-an-atm-machine.py
@@ -1,20 +1,32 @@
 class ATM:
-    def __init__(self):
-        self.cash = [0] * 5
-        self.values = [20, 50, 100, 200, 500]
 
-    def deposit(self, banknotes_count: List[int]) -> None:
-        for i, n in enumerate(banknotes_count):
-            self.cash[i] += n
+    def __init__(self):
+        self.notes = [20, 50, 100, 200, 500]
+        self.count = [0] * 5
+        
+
+    def deposit(self, banknotesCount: List[int]) -> None:
+        for i in range(len(self.notes)):
+            self.count[i] += banknotesCount[i]
 
     def withdraw(self, amount: int) -> List[int]:
-        res = []
-        for val, n in zip(self.values[::-1], self.cash[::-1]):
-            need = min(n, amount // val)
-            res = [need] + res
-            amount -= (need * val)
-        if amount == 0:
-            self.deposit([-x for x in res])
-            return res
-        else:
+        tracked = [0] * 5
+        
+        for i in reversed(range(len(self.notes))):
+            tracked[i] = min(self.count[i], amount // self.notes[i])
+            amount -= tracked[i] * self.notes[i]
+        
+        if amount:
             return [-1]
+        
+        for i in range(len(tracked)):
+            self.count[i] -= tracked[i]
+
+        return tracked
+        
+
+
+# Your ATM object will be instantiated and called as such:
+# obj = ATM()
+# obj.deposit(banknotesCount)
+# param_2 = obj.withdraw(amount)

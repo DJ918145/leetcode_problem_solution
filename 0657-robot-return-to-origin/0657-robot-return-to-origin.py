ae81ace3cf3739b3048e794b76d296a6ab2641b1
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-04-05 19:08:05 +0530

    Time: 0 ms (100%), Space: 12.6 MB (64.78%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -4,15 +4,5 @@ class Solution(object):
         :type moves: str
         :rtype: bool
         """
-        curr = [0, 0]
-        for move in moves:
-            if move=="R":
-                curr[0]+=1
-            elif move=="L":
-                curr[0]-=1
-            elif move=="U":
-                curr[1]+=1
-            else:
-                curr[1]-=1
-        return True if curr == [0,0] else False
+        return moves.count('U') == moves.count('D') and moves.count('R') == moves.count('L')
         

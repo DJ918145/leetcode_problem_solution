09dbe1f1eb8cc473df3569e33352f3aa70d55d9f
Jain Dhruv Vijaykumar <152017295+DJ918145@users.noreply.github.com>
2026-06-13 18:05:35 +0530

    Time: 23 ms (29.75%), Space: 12.4 MB (21.49%) - LeetHub

--- 3838-weighted-word-mapping/3838-weighted-word-mapping.py
+++ 3838-weighted-word-mapping/3838-weighted-word-mapping.py
@@ -3,12 +3,8 @@ class Solution(object):
         result = []
         
         for word in words:
-            total = 0
-            for ch in word:
-                total += weights[ord(ch) - ord('a')]
-            
-            mod = total % 26
-            mapped_char = chr(ord('z') - mod)
+            total_weight = sum(weights[ord(char) - ord('a')] for char in word)
+            mod_weight = total_weight % 26
+            mapped_char = chr(ord('z') - mod_weight)
             result.append(mapped_char)
-        
-        return ''.join(result)
+        return "".join(result)
